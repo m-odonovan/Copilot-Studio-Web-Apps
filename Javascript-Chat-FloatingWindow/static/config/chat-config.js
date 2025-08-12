c5206cb3b612464b6fafeeb45d3c82a594bdab8c
Michael O'Donovan <m_odonovan@hotmail.com>
2025-08-12 13:57:43 +0200

    Update chat-config.js

--- Javascript-Chat-FloatingWindow/static/config/chat-config.js
+++ Javascript-Chat-FloatingWindow/static/config/chat-config.js
@@ -1,6 +1,6 @@
 // Chat Configuration
 const CHAT_CONFIG = {
-  tokenEndpoint: 'https://c4259b43dd33e1d7a99c3805d2faca.57.environment.api.powerplatform.com/powervirtualagents/botsbyschema/cr245_subscriberSupportAgent/directline/token?api-version=2022-03-01-preview'
+  tokenEndpoint: '<directlinefullpath>'
 };
 
 // Global Variables
@@ -14,4 +14,5 @@ let isDragging = false;
 let dragStartX = 0;
 let dragStartY = 0;
 let chatStartX = 0;
+
 let chatStartY = 0;
